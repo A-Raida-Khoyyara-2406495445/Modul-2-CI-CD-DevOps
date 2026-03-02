a8be8e4b0788bb9489dac484f21759ace63c53fd
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-03-03 00:27:39 +0700

    delete unused import

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService{
--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryTest.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.Iterator;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarRepositoryTest {
