bb2ee23f1433c93d6b1e523889a933b1e3a37948
raidakhoyyara <raidakhoyyara247@gmail.com>
2026-03-02 23:17:33 +0700

    simplify update method in CarRepository to apply OCP

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -38,10 +38,8 @@ public class CarRepository {
     for(int i =0; i<carData.size(); i++){
       Car car = carData.get(i);
       if(car.getCarId().equals(id)){
-        car.setCarName(updatedCar.getCarName());
-        car.setCarColor(updatedCar.getCarColor());
-        car.setCarQuantity(updatedCar.getCarQuantity());
-        return car;
+        carData.set(i, updatedCar);
+        return updatedCar;
       }
     }
     return null;
@@ -51,3 +49,4 @@ public class CarRepository {
     carData.removeIf(car -> car.getCarId().equals(id));
   }
 }
+
